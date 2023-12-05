5ad4f432a15f944c19f41148f02450934f0d1a70
Julius Halank <julius.halank@uni-jena.de>
2023-12-05 11:38:52 +0100

    will work, no cap

---
+++ submissions/7_One-Sided_Comunication/b.cpp
@@ -0,0 +1,45 @@
+#include <mpi.h>
+#include <iostream>
+#include <cassert>
+
+bool success(int status);
+//TODO: Asserts for MPI functions
+int main(int argc, char *argv[])
+{
+    int l_err = MPI_Init(&argc, &argv);
+    assert(success(l_err));
+
+    int l_rank;
+    int l_comm_size;
+
+    l_err = MPI_Comm_rank(MPI_COMM_WORLD, &l_rank);
+    assert(success(l_err));
+    l_err = MPI_Comm_size(MPI_COMM_WORLD, &l_comm_size);
+    assert(success(l_err));
+    
+    MPI_Win l_win;
+    int l_data = 100;
+
+    MPI_Win_create(&l_data, 1*sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &l_win);
+
+    MPI_Win_fence( MPI_MODE_NOPRECEDE, l_win);
+
+    if (l_rank != 0)
+    {
+        MPI_Get(l_data, 1, MPI_INT, 0, 0, 1, MPI_INT, l_win);
+    }
+
+    MPI_Win_fence(0, l_win);
+    
+    if (l_rank == 1)
+    {
+        std::cout << "l_data of rank 1" << l_data << std::endl;
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+bool success(int status)
+{
+    return status == MPI_SUCCESS;
+}
