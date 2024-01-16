cc31f7666800c9784df00da936ff619c76638d4a
Minutenreis <Minutenreis@gmail.com>
2024-01-16 11:30:41 +0100

    Added Performance PNG

--- submissions/10_blocking/performance.py
+++ submissions/10_blocking/performance.py
@@ -1,9 +1,14 @@
 import sys
 from matplotlib import pyplot as plt
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 2 and len(sys.argv[1]) != 3:
     print("Usage: python performance.py <input_file>")
     sys.exit(1)
+    
+if len(sys.argv[1]) == 3:
+    output_file = sys.argv[2]
+else:
+    output_file = "performance.png"
 
 input_file = sys.argv[1]
 
@@ -17,7 +22,8 @@ with open(input_file, "r") as f:
 for i in range(1,len(header)):
     plt.plot([row[0] for row in data], [row[i] for row in data], label=header[i])
 plt.xlabel(header[0])
+plt.ylabel("GFLOPS")
 plt.legend()
 # plt.yscale("log")
 plt.xscale("log")
-plt.savefig("performance.png")
+plt.savefig(output_file)
