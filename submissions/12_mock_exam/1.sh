70c1c007235cba62811b874f567745bea09717ae
Justus Dreßler <minutenreis@gmail.com>
2024-02-19 11:17:46 +0100

    Update Shell Script

--- submissions/12_mock_exam/1.sh
+++ submissions/12_mock_exam/1.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --partition=s_hadoop
-#SBATCH --nodes=10
-#SBATCH --cpus-per-task=48
+#SBATCH --nodes=2
+#SBATCH --ntasks=20
 #SBATCH --job-name=quantum_job
+#SBATCH --output=quantum_job.out
+#SBATCH --error=quantum_job.err
 #SBATCH --time=10:00:00
 
-# meiner Meinung nach reicht `module load openmpi/4.0.3`
-module load mpi/openmpi/4.0.3-gcc-10.2.0
+module load mpi/openmpi/4.1.2-gcc-10.2.0
 
 mpicxx -o quantum_sim quantum_sim.cpp
-mpirun -np 480 ./quantum_sim
+mpirun -np 20 ./quantum_sim
